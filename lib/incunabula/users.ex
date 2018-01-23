8473162e81d258be44367f4da68cd7cf7545c1f1
Gordon Guthrie <gguthrie@basho.com>
2018-01-23 14:52:15 +0000

    tweaking user processing

--- lib/incunabula/users.ex
+++ lib/incunabula/users.ex
@@ -1,9 +1,29 @@
 defmodule Incunabula.Users do
 
   def get_users() do
+    usersandhashes = get_users_and_hashes()
+    for {user, _password} <- usersandhashes, do: user
+  end
+
+  def is_login_valid(username, password) do
+    usersandhashes = get_users_and_hashes()
+    case List.keyfind(usersandhashes, username, 0) do
+      nil             ->
+        false
+      {username, hash} ->
+        IO.inspect "checking password"
+        IO.inspect password
+        IO.inspect hash
+        IO.inspect Comeonin.Pbkdf2.checkpw(password, hash)
+        IO.inspect "returning"
+        # IO.inspect ret
+    end
+  end
+
+  defp get_users_and_hashes() do
     path = Path.join(:code.priv_dir(:incunabula), "users/users.config")
     {:ok, [users: users]} = :file.consult(path)
-    for {user, password} <- users, do: user
+    users
   end
 
 end
