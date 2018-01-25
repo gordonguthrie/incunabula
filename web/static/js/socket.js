37c522337ec32ba42a3315ddc85b8f16977e9734
Gordon Guthrie <gguthrie@basho.com>
2018-01-25 22:55:14 +0000

    channels working now

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -53,10 +53,15 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 
 socket.connect()
 
-// Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("book:mybook", {})
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
+var booktitle = document.getElementById("#bookslug")
+
+if (bookslug != null) {
+    // Now that you are connected, you can join channels with a topic:
+    let channel = socket.channel("book:${bookslug}", {})
+    channel.join()
+        .receive("ok", resp => { console.log("Joined successfully", resp) })
+        .receive("error", resp => { console.log("Unable to join", resp) })
+    channel.on("ping", ({count}) => console.log("pinging in socket", count))
+}
 
 export default socket
