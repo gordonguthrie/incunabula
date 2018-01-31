bc7890b1ea9308b6eec26593a40986d524d07b5c
Gordon Guthrie <gguthrie@basho.com>
2018-01-31 15:34:22 +0000

    remove dead code

--- web/channels/books_channel.ex
+++ web/channels/books_channel.ex
@@ -7,26 +7,10 @@ defmodule Incunabula.BooksChannel do
     {:ok, books, socket}
   end
 
-  def handle_in(msg, params, socket) do
-    {:noreply, socket}
-  end
-
   def handle_info(:ping, socket) do
     count = socket.assigns[:count] || 1
     push socket, "ping", %{count: count}
     {:noreply, assign(socket, :count, count + 1)}
-  end
-
-  # this clause handles an info message we get sent when we render HTML
-  # for the message channel via a controller/view
-  def handle_info({:plug_conn, :sent}, socket) do
-    {:noreply, socket}
-  end
-
-  # this clause handles an info message we get sent when we render HTML
-  # for the message channel via a controller/view
-  def handle_info({ref, {200, _, _}}, socket) when is_reference(ref) do
-    {:noreply, socket}
   end
 
 end
