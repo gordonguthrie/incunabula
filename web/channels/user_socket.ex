1337d6f7e2553356a13f091586d4d513fb8fff45
Gordon Guthrie <gguthrie@basho.com>
2018-02-01 15:16:12 +0000

    add authentication to the sockets

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -20,9 +20,17 @@ defmodule Incunabula.UserSocket do
   #
   # See `Phoenix.Token` documentation for examples in
   # performing token verification on connect.
-  def connect(_params, socket) do
-    {:ok, socket}
+  def connect(%{"token" => token} = _params, socket) do
+    IO.inspect "in channel connect"
+    case Phoenix.Token.verify(socket, "user socket", token, max_age: @max_age) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user_id, user_id)}
+      {:error, _reason} ->
+        :error
+    end
   end
+
+  def connect(_params, _socket), do: :error
 
   # Socket id's are topics that allow you to identify all sockets for a given user:
   #
--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -9,18 +9,27 @@ defmodule Incunabula.Auth do
 
   def call(conn, _opts) do
     user_id = get_session(conn, :user_id)
-    assign(conn, :current_user, user_id)
+    put_current_user(conn, user_id)
   end
 
   def authenticate_user(conn, _opts) do
-    if conn.assigns.current_user do
-      conn
-    else
-      conn
-      |> put_flash(:error, "You must be logged in to access that page")
-      |> redirect(to: "/")
-      |> halt()
+    cond do
+      user = conn.assigns.current_user ->
+        conn
+        |> put_current_user(user)
+    true ->
+        conn
+        |> put_flash(:error, "You must be logged in to access that page")
+        |> redirect(to: "/")
+        |> halt()
     end
   end
 
+  defp put_current_user(conn, user) do
+    IO.inspect user
+    token = Phoenix.Token.sign(conn, "user socket", user)
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
+  end
 end
