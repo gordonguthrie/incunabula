84b5c8fb6f0ab70232b5dee5ab9657af954aed91
Gordon Guthrie <gguthrie@basho.com>
2018-02-05 22:01:14 +0000

    The endpoint is now serving the vendor files
    BUT brunch is not creating them correctly so you need to fix that

--- lib/incunabula/endpoint.ex
+++ lib/incunabula/endpoint.ex
@@ -8,8 +8,8 @@ defmodule Incunabula.Endpoint do
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
   plug Plug.Static,
-    at: "/", from: :incunabula, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    at: "/", from: :incunabula, gzip: true,
+    only: ~w(css fonts images vendor js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
