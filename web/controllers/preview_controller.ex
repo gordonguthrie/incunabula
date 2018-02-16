2ae94b173b2cd319218198f1f825b2648909e5ed
Gordon Guthrie <gguthrie@basho.com>
2018-02-16 14:42:12 +0000

    add preview for reviews

--- web/controllers/preview_controller.ex
+++ web/controllers/preview_controller.ex
@@ -5,27 +5,34 @@ defmodule Incunabula.PreviewController do
 
   plug :authenticate_user when action in [:show]
 
-  def show(conn, %{"chapterslug" => ch_slug,
-                   "slug"        => slug}, _user) do
-    do_show(conn, slug, ch_slug, :chapter)
+  def show(conn, %{"reviewslug" => review_slug,
+                   "slug"       => slug}, _user) do
+    do_show(conn, slug, review_slug, :review)
   end
 
-  def show(conn, %{"chaffslug" => ch_slug,
+  def show(conn, %{"chaffslug" => chaff_slug,
                    "slug"      => slug}, _user) do
-    do_show(conn, slug, ch_slug, :chaff)
+    do_show(conn, slug, chaff_slug, :chaff)
   end
 
-  defp do_show(conn, slug, ch_slug, type) do
+  def show(conn, %{"chapterslug" => chapter_slug,
+                   "slug"        => slug}, _user) do
+    do_show(conn, slug, chapter_slug, :chapter)
+  end
+
+  defp do_show(conn, slug, frag_slug, type) do
     booksdir = Incunabula.Git.get_books_dir()
     preview_dir = case type do
                     :chapter -> "preview_html"
+                    :review  -> "review_html"
                     :summary -> "preview_html"
                     :chaff   -> "chaff_html"
                   end
     filename = case type do
-                 :chapter -> ch_slug <> ".html"
-                 :summary -> ch_slug <> ".summary.html"
-                 :chaff   -> ch_slug <> ".html"
+                 :chapter -> frag_slug <> ".html"
+                 :review  -> frag_slug <> ".html"
+                 :summary -> frag_slug <> ".summary.html"
+                 :chaff   -> frag_slug <> ".html"
                end
     file = Path.join([
       booksdir,
