884e96f921913dbed24bf39815b73ae585f65ef3
Gordon Guthrie <gguthrie@basho.com>
2018-02-16 14:45:41 +0000

    Add the various stage buttons for reviews and implented smart routing for reviews under the process
    The format of the reviews table has been adjusted to make it work on small devices.
    
    These changes have also been applied to books, chapters and chaffs - also the lock icon on chapters has been aligned with that on reviews
    
    Buttons to switch review statuses are build but not wired in yet

--- web/views/frag_view.ex
+++ web/views/frag_view.ex
@@ -1,4 +1,62 @@
 defmodule Incunabula.FragView do
   use Incunabula.Web, :view
 
+  def make_review_control("in review") do
+    """
+    <button class='ui labeled icon basic right floated fluid mini button' data-next='in reconciliation'>
+    <i class='arrow right icon'></i>
+    in review
+    </button>
+    """
+  end
+
+  def make_review_control("in reconciliation") do
+    """
+    <button class='ui labeled icon basic right floated fluid  mini button' data-next='reconcile'>
+    <i class='arrow right icon'></i>
+    in reconciliation
+    </button>
+    """
+  end
+
+  def make_review_control("closed") do
+    """
+    <button class='circular ui disabled basic right floated mini icon button'>
+    <i class='lock icon'></i>
+    </button>
+    """
+  end
+
+  def make_review_link(slug, review_slug, review_title, "in review") do
+    "<a href='/books/" <>
+      slug             <>
+      "/reviews/"      <>
+      review_slug      <>
+      "'>"             <>
+      review_title     <>
+      "</a>"
+  end
+
+  def make_review_link(slug, review_slug, review_title, "in reconciliation") do
+    "<a href='/books/"  <>
+      slug              <>
+      "/reviews/"       <>
+      review_slug       <>
+      "/reconciliation" <>
+      "'>"              <>
+      review_title      <>
+      "</a>"
+  end
+
+  def make_review_link(slug, review_slug, review_title, "closed") do
+    "<a href='/books/" <>
+      slug             <>
+      "/reviews/"      <>
+      review_slug      <>
+      "/preview"       <>
+      "'>"             <>
+      review_title     <>
+      "</a>"
+  end
+
 end
